dea626ace8f992bd3b156628bc465cb07add4071
Han Woo <metalalive2008@gmail.com>
2021-09-24 01:14:16 +0800

    clean up unecessary cookies set by CSRF middleware
    Signed-off-by: Han Woo <metalalive2008@gmail.com>

--- staff_portal/common/csrf/middleware.py
+++ staff_portal/common/csrf/middleware.py
@@ -52,49 +52,20 @@ class ExtendedCsrfViewMiddleware(CsrfViewMiddleware):
                 httponly=django_settings.CSRF_COOKIE_HTTPONLY,
                 samesite=django_settings.CSRF_COOKIE_SAMESITE,
             )
-            response.set_cookie(
-                key='csrf_cookie_name',  value=django_settings.CSRF_COOKIE_NAME,  max_age=tkn_age ,
-                domain=django_settings.CSRF_COOKIE_DOMAIN,
-                path=django_settings.CSRF_COOKIE_PATH,
-                secure=django_settings.CSRF_COOKIE_SECURE,
-                httponly=django_settings.CSRF_COOKIE_HTTPONLY,
-                samesite=django_settings.CSRF_COOKIE_SAMESITE,
-            )
             # Set the Vary header since content varies with the CSRF cookie.
             patch_vary_headers(response, ('Cookie',))
 
     def _reject(self, request, reason):
         json_mimetypes = ['application/json', 'application/x-ndjson', 'application/*']
-        result = accept_mimetypes_lookup(http_accept=request.headers['accept'],
-                expected_types=json_mimetypes)
+        result = accept_mimetypes_lookup(
+                http_accept=request.headers.get('accept', ''),
+                expected_types=json_mimetypes )
         if any(result): # json
             data = {NON_FIELD_ERRORS : [reason], }
             response = JsonResponse(data=data, status=403) # forbidden access
         else: # fall back to django default HTML response on CSRF failure
             response = super()._reject(request=request, reason=reason)
         return response
-
-
-#class ExtendedCsrfApiMiddleware(ExtendedCsrfViewMiddleware):
-#
-#    def _reject(self, request, reason):
-#        from rest_framework.settings    import api_settings as drf_settings
-#        from rest_framework.response    import Response as RestResponse
-#        from rest_framework             import status as RestStatus
-#        from rest_framework.renderers   import TemplateHTMLRenderer, JSONRenderer
-#        supported_renderer_classes = [TemplateHTMLRenderer, JSONRenderer]
-#        # may return json response if client required, default is HTML response
-#        renderers = [renderer() for renderer in supported_renderer_classes]
-#        conneg = drf_settings.DEFAULT_CONTENT_NEGOTIATION_CLASS()
-#        neg = conneg.select_renderer(request, renderers)
-#        accepted_renderer, accepted_media_type = neg
-#        if isinstance(accepted_renderer, TemplateHTMLRenderer):
-#            # fall back to django default HTML response on CSRF failure
-#            response = super()._reject(request=request, reason=reason)
-#        else: # json
-#            data = {drf_settings.NON_FIELD_ERRORS_KEY: [reason], }
-#            response = RestResponse(data=data, status=RestStatus.HTTP_403_FORBIDDEN)
-#        return response
 
 
 csrf_protect_m = method_decorator(decorator_from_middleware(ExtendedCsrfViewMiddleware))
