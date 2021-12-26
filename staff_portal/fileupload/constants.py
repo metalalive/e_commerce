8607ffc56103b1f53802918a6228411e65328768
Han Woo <metalalive2008@gmail.com>
2021-12-26 18:04:27 +0800

    remove fileupload app
    Signed-off-by: Han Woo <metalalive2008@gmail.com>

--- staff_portal/fileupload/constants.py
+++
@@ -1,6 +0,0 @@
-from common.util.python.fastapi.settings import settings as fa_settings
-
-ACCEPT_DUPLICATE = True
-ANONYMOUS_USER = '-1' # TODO, will be configurable parameter
-app_cfg = fa_settings.apps['fileupload']
-
