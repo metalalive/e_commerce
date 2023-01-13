0a06c1e6a4fc3e8d5793d0073b615d8395a51738
Han Woo <metalalive2008@gmail.com>
2023-01-13 15:56:13 +0800

    [media][transcoder][image] save metadata, commit transcoded file
    Signed-off-by: Han Woo <metalalive2008@gmail.com>

--- staff_portal/media/src/transcoder/video/metadata.c
+++ staff_portal/media/src/transcoder/video/metadata.c
@@ -13,7 +13,7 @@ static  void  atfp_video__dst_update_metadata__db_async_err(db_query_t *target, 
 {
     atfp_t *processor = (atfp_t *) target->cfg.usr_data.entry[0];
     json_object_set_new(processor->data.error, "model",
-            json_string("failed to update metadata of transcoded video"));
+            json_string("[video] failed to update metadata of transcoded video"));
     fprintf(stderr, "[transcoder][video][metadata] line:%d, job_id:%s, unknown db error \n",
             __LINE__, processor->data.rpc_receipt->job_id.bytes);
     processor->data.callback(processor);
