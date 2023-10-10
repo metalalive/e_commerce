bdbe3bb1173a2b2d616b157b9fdd1dd3b10e58a7
Han Woo <metalalive2008@gmail.com>
2023-10-10 22:20:49 +0800

    [order][usecase] feature: modify the code that validates billing / shipping DTO
    Signed-off-by: Han Woo <metalalive2008@gmail.com>

--- staff_portal/order/src/usecase/manage_order.rs
+++ staff_portal/order/src/usecase/manage_order.rs
@@ -5,9 +5,9 @@ use crate::AppSharedState;
 use crate::constant::ProductType;
 use crate::api::web::dto::{
     OrderCreateRespOkDto, OrderCreateRespErrorDto, OrderLinePayDto, PayAmountDto,
-    OrderCreateReqData, ShippingErrorDto, ContactErrorDto, ContactNameErrorReason,
-    PhoneNumNationErrorReason, PhoneNumberErrorDto
+    OrderCreateReqData, ShippingReqDto, BillingReqDto
 };
+use crate::model::{BillingModel, ShippingModel};
 use crate::repository::{AbsOrderRepo, AbsProductPriceRepo, AbstProductPolicyRepo};
 
 pub struct CreateOrderUseCase {
@@ -16,48 +16,35 @@ pub struct CreateOrderUseCase {
     pub repo_price: Box<dyn AbsProductPriceRepo>,
     pub repo_policy:Box<dyn AbstProductPolicyRepo>
 }
+
 impl CreateOrderUseCase {
     pub async fn execute(self, req:OrderCreateReqData) -> DefaultResult<OrderCreateRespOkDto, OrderCreateRespErrorDto>
     { // TODO, complete implementation
-        let mut error = OrderCreateRespErrorDto {
-            order_lines:None, billing:None, shipping: Some(ShippingErrorDto {
-                contact:None, address:None, option:None, nonfield:None})
+        let  (sh_d, bl_d, _ol_d) = (req.shipping, req.billing, req.order_lines);
+        let (_b, _s) = self.try_into_models(sh_d, bl_d)?;
+        let reserved_item = OrderLinePayDto {
+            seller_id: 389u32, product_id: 1018u64, product_type:ProductType::Item,
+            quantity: 9u32, amount: PayAmountDto {unit:4u32, total:35u32}
         };
-        let mut contact_err = ContactErrorDto {first_name:None,
-                    last_name:None, emails:None, phones:None, nonfield:None};
-        let _contact = &req.shipping.contact;
-        let err_name = _contact.first_name.is_empty();
-        let err_phone = (0.._contact.phones.len()).into_iter().map(
-            |idx| {
-                let item = &_contact.phones[idx];
-                if item.nation > 0 {
-                    None
-                } else {
-                    Some(PhoneNumberErrorDto {number:None, nation:
-                        Some(PhoneNumNationErrorReason::InvalidCode)})
-                }
-            }
-        ).collect::<Vec<Option<PhoneNumberErrorDto>>>();
-        if err_name {
-            contact_err.first_name = Some(ContactNameErrorReason::Empty);
-        }
-        if err_phone.iter().any(|d| d.is_some()) {
-            contact_err.phones = Some(err_phone);
-        }
-        if contact_err.first_name.is_none() && contact_err.phones.is_none()
-        {
-            let reserved_item = OrderLinePayDto {
-                seller_id: 389u32, product_id: 1018u64, product_type:ProductType::Item,
-                quantity: 9u32, amount: PayAmountDto {unit:4u32, total:35u32}
-            };
-            let obj = OrderCreateRespOkDto { order_id: "ty033u29G".to_string(),
-                usr_id: 789u32, time: 29274692u64, reserved_lines: vec![reserved_item],
-            };
-            Ok(obj)
-        } else {
-            let x = error.shipping.as_mut().unwrap();
-            x.contact = Some(contact_err);
-            Err(error)
-        }
+        let obj = OrderCreateRespOkDto { order_id: "ty033u29G".to_string(),
+            usr_id: 789u32, time: 29274692u64, reserved_lines: vec![reserved_item],
+        };
+        Ok(obj)
     } // end of fn execute
+
+    fn try_into_models(&self, sh_d:ShippingReqDto, bl_d:BillingReqDto)
+        -> DefaultResult<(BillingModel,ShippingModel), OrderCreateRespErrorDto>
+    {
+        let results = (BillingModel::try_from(bl_d), ShippingModel::try_from(sh_d));
+        if let (Ok(billing), Ok(shipping)) = results {
+            Ok((billing, shipping))
+        } else {
+            let mut obj = OrderCreateRespErrorDto { order_lines: None,
+                billing:None, shipping: None };
+            if let Err(e) = results.0 { obj.billing = Some(e); }
+            if let Err(e) = results.1 { obj.shipping = Some(e); }
+            Err(obj)
+        }
+    }
 } // end of impl CreateOrderUseCase
+
