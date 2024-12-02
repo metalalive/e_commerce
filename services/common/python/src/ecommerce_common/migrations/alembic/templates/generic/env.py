662e3ee62c3aafd07f0ef39a633647216bed907b
Ham <7197142+metalalive@users.noreply.github.com>
2024-12-02 11:44:00 +0800

    [storefront] build: continuous integration setup with GitHub Action
    - set up mariaDB docker , python and dependency package installation
    
    TODO
    - remove custom schema migration script, move back to alembic default commands
    - add test commands to this CI flow

--- services/common/python/src/ecommerce_common/migrations/alembic/templates/generic/env.py
+++ services/common/python/src/ecommerce_common/migrations/alembic/templates/generic/env.py
@@ -1,7 +1,9 @@
+import asyncio
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
 from sqlalchemy import pool
+from sqlalchemy.engine import Connection
+from sqlalchemy.ext.asyncio import async_engine_from_config
 
 # proxied from EnvironmentContext instance
 from alembic import context
@@ -61,6 +63,30 @@ def run_migrations_offline():
         context.run_migrations()
 
 
+def do_run_migrations(connection: Connection) -> None:
+    context.configure(
+        connection=connection,
+        target_metadata=target_metadata,
+        version_table=config.get_main_option("version_table"),
+    )
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+async def run_async_migrations() -> None:
+    connectable = async_engine_from_config(
+        config.get_section(config.config_ini_section, {}),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+        connect_args={'connect_timeout': 50, 'net_read_timeout', 45},
+    )
+
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
+
+    await connectable.dispose()
+
+
 def run_migrations_online():
     """Run migrations in 'online' mode.
 
@@ -68,20 +94,7 @@ def run_migrations_online():
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
-
-    with connectable.connect() as connection:
-        context.configure(
-            connection=connection,
-            target_metadata=target_metadata,
-            version_table=config.get_main_option("version_table"),
-        )
-        with context.begin_transaction():
-            context.run_migrations()
+    asyncio.run(run_async_migrations())
 
 
 if context.is_offline_mode():
