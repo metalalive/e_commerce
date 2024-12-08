3adfbda9ea62040e7769e3e49f9ba4f20190a5b0
T.H <7197142+metalalive@users.noreply.github.com>
2024-12-08 16:02:34 +0800

    [user-management] build / refactor : continuous integration with Github Action
    Signed-off-by: T.H <7197142+metalalive@users.noreply.github.com>

--- services/user_management/settings/migration.py
+++ services/user_management/settings/migration.py
@@ -1,6 +1,7 @@
 from .common import *
 
 DATABASES["default"].update(DATABASES["usermgt_service"])
-DATABASES["default"]['HOST'] = '127.0.0.1'
+for d in DATABASES.values():
+    d["HOST"] = "127.0.0.1"
 
 render_logging_handler_localfs(BASE_DIR.joinpath("tmp/log/dev"))
