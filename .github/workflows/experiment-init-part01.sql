7301a0d5e090ba26cba511c3556d6042f7bc9887
Ham <7197142+metalalive@users.noreply.github.com>
2024-12-11 11:48:11 +0800

    Update experiment-init-part01.sql

--- .github/workflows/experiment-init-part01.sql
+++ .github/workflows/experiment-init-part01.sql
@@ -3,8 +3,9 @@ CREATE DATABASE `test_db_whatever_replica`  DEFAULT CHARACTER SET utf8mb4 COLLAT
 GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, GRANT OPTION ON `test_db_whatever_replica`.* TO 'DB_USERNAME'@'localhost';
 
 -- following grant commands does not work in default mariaDB docker
+
 -- GRANT SELECT ON `mysql`.`user` TO 'DB_USERNAME'@'localhost';
 -- GRANT SELECT ON `mysql`.`db` TO 'DB_USERNAME'@'localhost';
+-- GRANT SHOW DATABASES ON *.* TO 'DB_USERNAME'@'localhost';
 
-GRANT SHOW DATABASES ON *.* TO 'DB_USERNAME'@'localhost';
 FLUSH PRIVILEGES;
