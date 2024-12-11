f547374468e13ec89321f9d9277f07e68bb95ed7
Ham <7197142+metalalive@users.noreply.github.com>
2024-12-11 11:37:36 +0800

    Update experiment-init-part01.sql

--- .github/workflows/experiment-init-part01.sql
+++ .github/workflows/experiment-init-part01.sql
@@ -1,3 +1,6 @@
 CREATE DATABASE `test_db_whatever_replica`  DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 
 GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, GRANT OPTION ON `test_db_whatever_replica`.* TO 'DB_USERNAME'@'localhost';
+
+GRANT SELECT ON `mysql`.`user` TO 'DB_USERNAME'@'localhost';
+GRANT SELECT ON `mysql`.`db` TO 'DB_USERNAME'@'localhost';
