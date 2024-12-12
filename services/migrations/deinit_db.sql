6024769233bf2ef373a756084ffee281246b39c1
T.H <7197142+metalalive@users.noreply.github.com>
2024-12-13 00:20:14 +0800

    [order] build / refactor: reuse steps in github action CI flow
    Signed-off-by: T.H <7197142+metalalive@users.noreply.github.com>

--- services/migrations/deinit_db.sql
+++ services/migrations/deinit_db.sql
@@ -1,15 +1,6 @@
-
 REVOKE SELECT, INSERT, UPDATE, DELETE ON `test_ecommerce_media`.*   FROM 'media_admin'@'localhost';
-
 REVOKE SELECT, INSERT, UPDATE, DELETE ON `ecommerce_media`.*    FROM  'media_admin'@'localhost';
-REVOKE SELECT, INSERT, UPDATE, DELETE ON `ecommerce_order`.*    FROM  'order_admin'@'localhost';
-
 REVOKE SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, GRANT OPTION ON `test_ecommerce_media`.* FROM 'restauTestDBA'@'localhost';
-REVOKE SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, GRANT OPTION ON `test_ecommerce_order`.* FROM 'ecomSite2TestAdmin'@'localhost';
-
 REVOKE SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, GRANT OPTION ON `ecommerce_media`.* FROM 'restauDBA'@'localhost';
-REVOKE SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, GRANT OPTION ON `ecommerce_order`.* FROM 'ecomSite2DBA'@'localhost';
-
 DROP DATABASE `ecommerce_media`;
-DROP DATABASE `ecommerce_order`;
 
--- services/migrations/init_db.sql
+++ services/migrations/init_db.sql
@@ -1,15 +1,6 @@
-
 CREATE DATABASE `ecommerce_media`       DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
-CREATE DATABASE `ecommerce_order`       DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
-
 GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, GRANT OPTION ON `test_ecommerce_media`.* TO 'restauTestDBA'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, GRANT OPTION ON `test_ecommerce_order`.* TO 'ecomSite2TestAdmin'@'localhost';
-
 GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, GRANT OPTION ON `ecommerce_media`.* TO 'restauDBA'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, GRANT OPTION ON `ecommerce_order`.* TO 'ecomSite2DBA'@'localhost';
-
 GRANT SELECT, INSERT, UPDATE, DELETE ON `ecommerce_media`.*    TO 'media_admin'@'localhost';
-GRANT SELECT, INSERT, UPDATE, DELETE ON `ecommerce_order`.*    TO 'order_admin'@'localhost';
-
 GRANT SELECT, INSERT, UPDATE, DELETE ON `test_ecommerce_media`.*     TO 'media_admin'@'localhost';
 
