e661286c3377cf66dfdd9a25f129cc00ea55a846
T.H. <7197142+metalalive@users.noreply.github.com>
2024-12-20 14:42:51 +0800

    [media] misc: reduce test cases in one program for github action
    Signed-off-by: T.H. <7197142+metalalive@users.noreply.github.com>

--- services/media/test/unit/main.c
+++ services/media/test/unit/main.c
@@ -83,9 +83,11 @@ int main(int argc, char **argv) {
      * */
 
     add_suite(suite, app_transcoder_storage_tests());
-    add_suite(suite, app_transcoder_removal_tests());
 
     /*
+
+    add_suite(suite, app_transcoder_removal_tests());
+
     add_suite(suite, app_transcoder_validation_tests());
     add_suite(suite, app_transcoder_file_processor_tests());
     add_suite(suite, app_transcoder_video_storage_tests());
