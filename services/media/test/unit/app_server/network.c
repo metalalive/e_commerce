02d5da0d636f32ae3db130c0cf2bea96329ba688
T.H. <7197142+metalalive@users.noreply.github.com>
2024-12-21 18:41:37 +0800

    [media] test / bugfix: fetch correct IP from addr info chain required in test
    Signed-off-by: T.H. <7197142+metalalive@users.noreply.github.com>

--- services/media/test/unit/app_server/network.c
+++ services/media/test/unit/app_server/network.c
@@ -1,6 +1,21 @@
 #include <cgreen/cgreen.h>
 #include "network.h"
 
+static struct addrinfo * ut_search_addr_info(struct addrinfo *ai_chain, const char *ip) {
+    struct addrinfo *ai_chosen = NULL;
+    char actual_ip[INET_ADDRSTRLEN];
+    for(struct addrinfo *ai_curr = ai_chain; ai_curr; ai_curr = ai_curr->ai_next) {
+        inet_ntop(AF_INET, &((struct sockaddr_in *)ai_curr->ai_addr)->sin_addr,
+                (void *)&actual_ip[0], sizeof(actual_ip));
+        printf("[debug] resolve_net_addr_test: actual_ip : %s \n", &actual_ip[0]);
+        if(!strcmp(ip, &actual_ip[0])) {
+            ai_chosen = ai_curr;
+            break;
+        }
+    }
+    return ai_chosen;
+}
+
 Ensure(resolve_net_addr_test) {
     struct addrinfo *ai_chain = NULL, *ai_chosen = NULL;
     uint16_t expect_port = 8020;
@@ -8,18 +23,9 @@ Ensure(resolve_net_addr_test) {
     assert_that(ai_chain, is_equal_to(NULL));
     ai_chain = resolve_net_addr(SOCK_DGRAM, IPPROTO_UDP, "localhost", expect_port);
     assert_that(ai_chain, is_not_equal_to(NULL));
-    char actual_ip[INET_ADDRSTRLEN];
-    for(struct addrinfo *ai_curr = ai_chain; ai_curr; ai_curr = ai_curr->ai_next) {
-        inet_ntop(AF_INET, &((struct sockaddr_in *)ai_curr->ai_addr)->sin_addr,
-                (void *)&actual_ip[0], sizeof(actual_ip));
-        printf("[debug] resolve_net_addr_test: actual_ip : %s \n", &actual_ip[0]);
-        if(!strcmp("127.0.0.1", &actual_ip[0])) {
-            ai_chosen = ai_curr;
-            break;
-        }
-    }
+    ai_chosen = ut_search_addr_info(ai_chain, "127.0.0.1");
     assert_that(ai_chosen, is_not_null);
-    uint16_t actual_port = htons(((struct sockaddr_in *)ai_chain->ai_addr)->sin_port);
+    uint16_t actual_port = htons(((struct sockaddr_in *)ai_chosen->ai_addr)->sin_port);
     assert_that(expect_port, is_equal_to(actual_port));
     freeaddrinfo(ai_chain);
 } // end of resolve_net_addr_tests
@@ -40,16 +46,16 @@ Ensure(listener_access_test) {
     assert_that(found, is_null);
     ai = resolve_net_addr(SOCK_STREAM, IPPROTO_TCP, expect_host, expect_port);
     assert_that(ai, is_not_equal_to(NULL));
-    ai = ai->ai_next;
-    assert_that(ai, is_not_equal_to(NULL));
+    struct addrinfo *ai_chosen = ut_search_addr_info(ai, "127.0.0.1");
+    assert_that(ai_chosen, is_not_equal_to(NULL));
 
-    uv_tcp_t *nt_handle = create_network_handle(uv_default_loop(), ai, _dummy_cb_on_nt_accept, 64);
+    uv_tcp_t *nt_handle = create_network_handle(uv_default_loop(), ai_chosen, _dummy_cb_on_nt_accept, 64);
     assert_that(nt_handle, is_not_null);
     _new = create_new_listener((uv_handle_t *)nt_handle);
     assert_that(_new, is_not_null);
     h2o_append_to_null_terminated_list((void ***)&listeners, (void *)_new);
     assert_that(_new, is_equal_to(listeners[0]));
-    found = find_existing_listener(listeners, ai);
+    found = find_existing_listener(listeners, ai_chosen);
     assert_that(found, is_not_equal_to(NULL));
     assert_that(found, is_equal_to(listeners[0]));
     // ----
