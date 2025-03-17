fc628e6682e2ddedb6ba74b9cbf2f4ba6b21a91b
T.H. <7197142+metalalive@users.noreply.github.com>
2025-03-17 23:58:37 +0800

    [product] refactor: remove deprecated v1 codebase
    Signed-off-by: T.H. <7197142+metalalive@users.noreply.github.com>

--- services/product/v1.0.1/settings/development.py
+++
@@ -1,5 +0,0 @@
-# re-import the parameters from common.py , it is acceptable if some variables
-# are unused at here
-from .common import *  # noqa: F403
-
-render_logging_handler_localfs(BASE_DIR.joinpath("tmp/log/dev"))  # noqa: F405
--- services/product/v1.0.1/src/product/migrations/deinit_db.sql
+++
@@ -1,5 +0,0 @@
-REVOKE SELECT, INSERT, UPDATE, DELETE ON `ecommerce_product`.*  FROM  'app-user'@'localhost';
-REVOKE SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, GRANT OPTION ON `test_ecommerce_product`.* FROM 'test-dba'@'localhost';
-REVOKE SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, GRANT OPTION ON `ecommerce_product`.* FROM 'dev-dba'@'localhost';
-DROP DATABASE `ecommerce_product` ;
-
