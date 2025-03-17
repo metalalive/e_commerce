2a24865c7c2f6ae99e7a8f5c65844e4c0f93e910
Ham <7197142+metalalive@users.noreply.github.com>
2025-03-18 01:00:47 +0800

    [Product] refactor: link deprecated v1 codebase from archived git repo (#42)

--- services/product/v1.0.1/settings/migration.py
+++
@@ -1,6 +0,0 @@
-# re-import the parameters from common.py , it is acceptable if some variables
-# are unused at here
-from .common import *  # noqa: F403
-
-DATABASES["default"] = DATABASES["product_dev_service"]  # noqa: F405
-render_logging_handler_localfs(BASE_DIR.joinpath("tmp/log/dev"))  # noqa: F405
--- services/product/v1.0.1/settings/production.py
+++
@@ -1,6 +0,0 @@
-# re-import the parameters from common.py , it is acceptable if some variables
-# are unused at here
-from .common import *  # noqa: F403
-
-DEBUG = False
-render_logging_handler_localfs(BASE_DIR.joinpath("tmp/log/production"))  # noqa: F405
